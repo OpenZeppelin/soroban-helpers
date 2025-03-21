534d81572e20ef9ef459c22f9aa19e0c0dbe5d95
Luis Urrutia <luis@urrutia.me>
2025-03-21 16:11:00 +0100

    test(soroban-rs): allow unused imports because re-export for other mods

--- crates/soroban-rs/src/mock/mod.rs
+++ crates/soroban-rs/src/mock/mod.rs
@@ -5,12 +5,14 @@ pub mod transaction;
 pub mod account;
 
 // Re-export transaction mock functions
+#[allow(unused_imports)]
 pub use transaction::{
     mock_transaction, mock_transaction_response, mock_simulate_tx_response, 
     mock_transaction_envelope, mock_transaction_response_with_return_value,
 };
 
 // Re-export account mock functions
+#[allow(unused_imports)]
 pub use account::{
     mock_account_entry, mock_contract_id, mock_env, 
     all_signers, mock_signer1, mock_signer3,
